642ac89834e277d156cb0ba3adb51b0a56650ae8
Unknown <t5maju03@students.oamk.fi>
2017-11-01 11:02:58 +0200

    Array tehty

--- harjoitus.php
+++ harjoitus.php
@@ -6,5 +6,17 @@
   </head>
   <body>
     <h1>moro</h1>
+    <p>Seuraava taulukko on luotu php:llä</p>
+    <?php
+    $nimet=array(
+      array("Etunimi"=>'Ville',"Sukunimi"=>'Virta')
+      array("Etunimi"=>'Kalle',"Sukunimi"=>'Järvi')
+      array("Etunimi"=>'Liisa',"Sukunimi"=>'Virta')
+      array("Etunimi"=>'Marja',"Sukunimi"=>'Joki')
+    );
+    foreach ($nimet as $rivi) {
+      echo $rivi['Etunimi'];
+    }
+    ?>
   </body>
 </html>
